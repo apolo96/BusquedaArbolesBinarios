d404e34ceae35fe5b52c162311b94191d4ee516f
apolo96 <andresfpvclap@gmail.com>
2017-03-22 13:51:42 -0500

    Add code comments

--- src/busquedaarbolesbinarios/Arbol.java
+++ src/busquedaarbolesbinarios/Arbol.java
@@ -15,20 +15,49 @@ import java.util.Scanner;
  */
 public class Arbol {
     
+    /**
+     * Contiene la estructura del arbol
+     * en una matriz
+     */
     private int arbol[][];
     
+    /**
+     * Este vector contiene los elementos del Arbol
+     * de forma ordenada segun el metodo de busqueda
+     */
     private ArrayList<Integer> vector;
     
+    /**
+     * Objeto para leer los valores que ingrese el usuario
+     */
     private final Scanner sc = new Scanner(System.in);
     
+    /**
+     * Profundidad del arbol igual al,
+     * numero de filas de la matriz
+     */
     public int niveles;
     
+    /**
+     * El numero de ramas es igual a la cantidad 
+     * de hijos finales del arbol, define el ancho
+     * del arbol
+     */
     public int ramas;
         
+    /**
+     * Crea y define el tamaño del arbol,
+     * (Por cada nuevo nivel agregado sin contar el nivel de la raiz
+     * las ramas del arbol crecen por 2)
+     */
     public void crear(){
         this.arbol =  new int[niveles][ramas];
     }
     
+    /**
+     * Carga los elementos del arbol segun el tamaño definido,
+     *
+     */
     public void cargarNodos(){
         
         int ultimo  = niveles - 1;
@@ -58,6 +87,9 @@ public class Arbol {
         }         
     }
     
+    /**
+     * Busca por anchura los elementos del arbol y los ordena en un vector
+     */
     public void busquedaAmplitud(){        
         vector =  new ArrayList<Integer>();
         for(int f=0; f < niveles; f++){
@@ -70,6 +102,9 @@ public class Arbol {
         }
     }
     
+    /**
+     * Busca por profundidad los elementos del arbol y los ordena en un vector
+     */
     public void busquedaProfundidad(){
         vector =  new ArrayList<Integer>();
         for(int c=0; c < ramas; c++){  
@@ -82,6 +117,9 @@ public class Arbol {
         }  
     }
     
+    /**
+     * Muestra en pantalla los elementos del vector ordenado
+     */
     public void imprimir(){
         for( Integer element : vector){            
             if(element != 0)System.out.print(element+"-"); 
